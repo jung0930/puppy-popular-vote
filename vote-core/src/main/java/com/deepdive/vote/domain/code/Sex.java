6045af1cdec12289e603f96d289f53d1e0dbf858
jung0930 <dev.jung0930@gmail.com>
2023-05-20 04:31:29 +0900

    fix : 디렉토리 변경

---
+++ vote-core/src/main/java/com/deepdive/vote/domain/code/Sex.java
@@ -0,0 +1,16 @@
+package com.deepdive.vote.domain.code;
+
+public enum Sex {
+
+    MALE("M", "수컷"),
+    FEMALE("F", "암컷");
+
+    private String value;
+    private String description;
+
+    Sex(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+}
