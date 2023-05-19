033428349dfbad407c19ab6d51cacb82d0a66b03
jung0930 <dev.jung0930@gmail.com>
2023-05-20 04:32:23 +0900

    feat : API 모듈 h2 디비 설정

---
+++ vote-api/src/main/java/com/deepdive/vote/VoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.deepdive.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteApiApplication.class, args);
+    }
+
+}
